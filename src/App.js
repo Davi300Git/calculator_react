a26c2d12a387ce8db73f66e539c60840296a28ce
Davi Sandro <davisandro300@gmail.com>
2023-01-29 17:01:10 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World, this is My Calculator in React 
+    </div>
+  );
+}
+
+export default App;
